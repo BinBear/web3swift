eccc6545e297356b0bb567f7ccac7b9023f0fea0
BinBear <vin404@outlook.com>
2025-04-24 20:48:09 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.4.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.4"),
-        .package(name: "secp256k1", url: "https://github.com/21-DOT-DEV/swift-secp256k1", from: "0.18.0"),
+        .package(name: "secp256k1", url: "https://github.com/21-DOT-DEV/swift-secp256k1", exact: "0.18.0"),
     ],
     targets: [
         .target(
