056dcd9127b18382030f9605de334c0616bf3e51
BinBear <vin404@outlook.com>
2025-04-24 19:41:12 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
         .library(name: "web3swift", targets: ["web3swift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMinor(from: "5.4.0")),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.5.1"),
-        .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift", .upToNextMinor(from: "0.10.0")),
+        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.4.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.8.0")),
+        .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift", .upToNextMajor(from: "0.10.0")),
     ],
     targets: [
         .target(
